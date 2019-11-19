a5aed64dd932f33ca7e1f42943c380d8e6865f14
Koen <koenvwel@gmail.com>
2019-11-19 13:03:51 +0100

    Set up User Retrofit Service
    Need DI

--- app/src/main/java/com/kvw/technicaltestmediamonks/ui/users/UsersViewModel.kt
+++ app/src/main/java/com/kvw/technicaltestmediamonks/ui/users/UsersViewModel.kt
@@ -3,14 +3,19 @@ package com.kvw.technicaltestmediamonks.ui.users
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kvw.technicaltestmediamonks.models.User
+import com.kvw.technicaltestmediamonks.services.UserService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class UsersViewModel : ViewModel() {
+    lateinit var userService: UserService //TODO Get this through DI
 
     private val _users = MutableLiveData<List<User>>(emptyList())
     val users: LiveData<List<User>> get() = _users
 
     init {
-        TODO("fill _users")
+        viewModelScope.launch(Dispatchers.IO) { _users.postValue(userService.getAll()) }
     }
 }
