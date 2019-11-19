58367200b0dc2a58bba6abf3259beed20592d79c
Koen <koenvwel@gmail.com>
2019-11-19 18:10:52 +0100

    Create Model RecyclerView and Adapter

---
+++ app/src/main/java/com/kvw/technicaltestmediamonks/ui/albumphotos/AlbumPhotosAdapter.kt
@@ -0,0 +1,32 @@
+package com.kvw.technicaltestmediamonks.ui.albumphotos
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.kvw.technicaltestmediamonks.R
+import com.kvw.technicaltestmediamonks.models.Photo
+import kotlinx.android.synthetic.main.item_albumphotos_photo.view.*
+
+class AlbumPhotosAdapter(private val photos: List<Photo>) :
+    RecyclerView.Adapter<AlbumPhotosAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_albumphotos_photo, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = photos.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        with(holder.itemView){
+            textView_itemPhoto_title.text = photos[position].title
+
+            Glide.with(this).load(photos[position]).into(imageView_itemPhoto_photo)
+        }
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
