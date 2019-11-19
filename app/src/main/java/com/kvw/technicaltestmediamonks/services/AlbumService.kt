5fc2ab82d568f4f6d380f7f72ae4c9e60753dd70
Koen <koenvwel@gmail.com>
2019-11-19 17:48:13 +0100

    Fix AlbumEndpoint

--- app/src/main/java/com/kvw/technicaltestmediamonks/services/AlbumService.kt
+++ app/src/main/java/com/kvw/technicaltestmediamonks/services/AlbumService.kt
@@ -2,9 +2,9 @@ package com.kvw.technicaltestmediamonks.services
 
 import com.kvw.technicaltestmediamonks.models.Album
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface AlbumService {
-    @GET("/albums?userId={userId")
-    suspend fun getByUserId(@Path("userId") userId: Int): List<Album>
+    @GET("/albums")
+    suspend fun getByUserId(@Query("userId") userId: Int): List<Album>
 }
--- app/src/main/java/com/kvw/technicaltestmediamonks/ui/user_albums/UserAlbumsFragment.kt
+++ app/src/main/java/com/kvw/technicaltestmediamonks/ui/user_albums/UserAlbumsFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import com.kvw.technicaltestmediamonks.R
+import kotlinx.android.synthetic.main.user_albums_fragment.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
@@ -29,7 +31,12 @@ class UserAlbumsFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        // TODO: Use the ViewModel
+        userAlbumsViewModel.albums.observe(this, Observer { albumList ->
+            recyclerView_userAlbums_albums.swapAdapter(
+                UserAlbumsAdapter(albumList) {},
+                true
+            )
+        })
     }
 
 }
