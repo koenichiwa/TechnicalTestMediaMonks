94cdf31b10a3c6ddd05fb2ae6d3607c408a7c700
Koen <koenvwel@gmail.com>
2019-11-20 01:03:38 +0100

    Set up Actionbar

--- app/src/main/java/com/kvw/technicaltestmediamonks/MainActivity.kt
+++ app/src/main/java/com/kvw/technicaltestmediamonks/MainActivity.kt
@@ -2,12 +2,25 @@ package com.kvw.technicaltestmediamonks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController: NavController
+        get() = Navigation.findNavController(this, R.id.fragment_main_navHost)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        setupActionBarWithNavController(navController)
     }
 
+    override fun onSupportNavigateUp() = navController.navigateUp()
 }
